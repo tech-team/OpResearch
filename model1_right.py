c3094d7be8ac01b220d90455d58baf1441bf6915
igorcoding <igorcoding@gmail.com>
2014-10-16 22:40:09 +0400

    Some setup

---
+++ model1_right.py
@@ -0,0 +1,15 @@
+
+
+def solve(la, nu_avg, N):
+    """
+
+    :param la: lambda
+    :type la: float
+
+    :param nu_avg: nu vector
+    :type nu_avg: tuple
+
+    :param N: N
+    :type N int
+    """
+    pass
---
+++ model2_left.py
@@ -0,0 +1,15 @@
+
+
+def solve(mu, nu, N_avg):
+    """
+
+    :param mu: lambda
+    :type mu: float
+
+    :param nu: nu vector
+    :type nu: float
+
+    :param N_avg: N
+    :type N_avg int
+    """
+    pass
