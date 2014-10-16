856beeeb57cf851d10be36bdcb5ffa2b027c4508
igorcoding <igorcoding@gmail.com>
2014-10-16 23:26:10 +0400

    Done model2_right

--- model2_right.py
+++ model2_right.py
@@ -1,6 +1,28 @@
+from pprint import pprint
 
 
-def solve(mu, nu, N_avg):
+def _mu_i(mu, Nk, N_avg, i):
+    return mu * min(Nk, N_avg - i)
+
+
+def _nu_i(nu, Np, i):
+    return nu * min(Np, i)
+
+
+def _compute_coefs(mu, nu, N_avg, Nk, Np):
+    coefs = []
+    addings = [1]
+
+    for i in xrange(N_avg, 0, -1):
+        nu_i = _nu_i(nu, Np, i)
+        mu_i = _mu_i(mu, Nk, N_avg, i-1)
+
+        coefs.append(nu_i / mu_i)
+        addings.append(coefs[-1] * addings[-1])
+
+    return coefs, addings
+
+def solve(mu, nu, N_avg, Nk, Np):
     """
 
     :param mu: mu
@@ -12,4 +34,28 @@ def solve(mu, nu, N_avg):
     :param N_avg: N
     :type N_avg int
     """
-    pass
+
+    p = []
+    coefs, addings = _compute_coefs(mu, nu, N_avg, Nk, Np)
+
+    p.append(1.0 / sum(addings))
+
+    for i in xrange(0, N_avg):
+        p_i = p[-1] * coefs[i]
+        p.append(p_i)
+
+    p = list(reversed(p))
+
+    return p
+
+
+if __name__ == "__main__":
+    mu = 0.1
+    nu = 1
+    N_avg = 10
+    Nk = 3
+    Np = 4
+
+    p = solve(mu, nu, N_avg, Nk, Np)
+    pprint(p)
+    print "Sum(p) = %f" % sum(p)
